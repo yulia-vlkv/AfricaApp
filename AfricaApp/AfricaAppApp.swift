35e61c4dcf3e5fd60843cb0e9355c9e8bb1db9c3
Iuliia Vlkv <yu.wlkw@gmail.com>
2022-07-04 19:04:41 +0200

    Initial Commit

---
+++ AfricaApp/AfricaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaAppApp.swift
+//  AfricaApp
+//
+//  Created by Iuliia Volkova on 04.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
